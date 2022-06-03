89acb25e970474ddb892c2f392ecacb961b98149
Kai-Deux <rohisharamauthar@gmail.com>
2022-06-03 06:19:06 -0700

    Task 6: Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
