96390881f936da2044c5896e768a37e6762f26ae
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-06-03 14:56:48 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,55 +1,28 @@
 #include <stdio.h>
 
-
-
 /**
-
  * main - Entry point
-
  *
-
  * Return: 0
-
  */
-
 int main(void)
-
 {
-
 	char a = 'a';
 
-
-
 	while (a <= 'z')
-
 	{
-
 		putchar(a);
-
 		a++;
-
 	}
-
-
 
 	a = 'A';
 
-
-
 	while (a <= 'Z')
-
 	{
-
 		putchar(a);
-
 		a++;
-
 	}
 
-
-
 	putchar('\n');
-
 	return (0);
-
 }
