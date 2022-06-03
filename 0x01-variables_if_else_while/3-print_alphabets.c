7438f00064efbfb657ef4e3b703df3d9235ade68
Kai-Deux <rohisharamauthar@gmail.com>
2022-06-03 05:55:43 -0700

    Task 3: alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: 0
+
+ */
+
+int main(void)
+
+{
+
+	char a = 'a';
+
+
+
+	while (a <= 'z')
+
+	{
+
+		putchar(a);
+
+		a++;
+
+	}
+
+
+
+	a = 'A';
+
+
+
+	while (a <= 'Z')
+
+	{
+
+		putchar(a);
+
+		a++;
+
+	}
+
+
+
+	putchar('\n');
+
+	return (0);
+
+}
