e916ead563b3689d4ce7245955d85ba6ac2d3c2b
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-06-07 13:05:14 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,25 @@
-#include "main.h"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+#include <unistd.h>
+
+int _putchar(char c)
 {
-	char *sh = "main";
+	return (write(1, &c, 1));
+}
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
+int main ()
+{
 
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+
+return 0;
+
+
 }
