4980274e11010e41f778550e9d02070a4aac54e8
Kai-Deux <rohisharamauthar@gmail.com>
2022-06-07 06:40:19 -0700

    Task 0. Multiple mains

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+0. In most projects, we often give you only one main file to test with. For example, this main file is a test for a postitive_or_negative() function similar to the one you worked with in an earlier C project
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _putchar(char c);
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif
+
