991457e32217faafde7e505827805744222bdf5d
Kai-Deux <rohisharamauthar@gmail.com>
2022-06-13 09:24:49 -0700

    Task 1:  Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+
+ * swap_int - swap the valuses of two integers a and b
+
+ *@a: A pointer to an int that will be updated
+
+ *@b: B pointer to an int that will be updated
+
+ * Return: void that means our answer is correct
+
+ */
+
+
+
+void swap_int(int *a, int *b)
+
+{
+
+int f;
+
+
+
+f = *a;
+
+*a = *b;
+
+*b = f;
+
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,5 @@
 # 0x05. C - Pointers, arrays and strings
 0. Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
     - Prototype: void reset_to_98(int *
+1. Write a function that swaps the values of two integers.
+	- Prototype: void swap_int(int *a, int *b);
