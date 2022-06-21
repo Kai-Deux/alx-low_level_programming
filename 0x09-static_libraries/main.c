d1a30a9cb680e8fce5e730d4bde3f616967488ff
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-06-22 00:25:17 +0200

    Create main.c

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,10 @@
+#include main.h
+/**
+ * main - Entry point
+ * Return: 0.
+ */
+int main(void)
+{
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
+}
