611043afe5f81ed96d90a7774eb27161529ca06b
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-06-21 18:17:39 +0200

    updated Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,15 @@
 #include "main.h"
-
 /**
- * _strlen_recursion - returns the length of a string
- * @s: string to calculate the length of
- *
- * Return: length of the string
+ *_print_rev_recursion - prints a string in reverse
+ *@s: pointer block of memory to fill
+ *Return: void
  */
-int _strlen_recursion(char *s)
+
+void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
+if (*s) /*If statement*/
+{
+_print_rev_recursion(s + 1); /*add s*/
+_putchar(*s); /*print s*/
+}
 }
