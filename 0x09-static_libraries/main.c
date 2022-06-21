b4162b12fbce23c7392e7e78881f0f434a2e3e14
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-06-22 01:16:16 +0200

    Update main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,11 +1,7 @@
 #include "main.h"
 
-/**
- * main - Entry point
- * Return: 0.
- */
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-return (0);
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
 }
