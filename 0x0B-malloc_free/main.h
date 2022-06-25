238003deeca07256bf2c4d255bb7e644f3e1c4fa
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-06-25 09:47:03 +0200

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,9 +1,5 @@
-#ifndef MAIN_H
-
-#define MAIN_H
-
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
