8612c61728f303cce7bff7a971dfc1282035c63b
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-06-30 12:43:59 +0200

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * _puts - prints a string, followed by a new line,
  * @str: pointer to the string to print
@@ -12,8 +12,8 @@ void _puts(char *str)
 int i = 0;
 while (str[i])
 {
-	_putchar(str[i]);
-	i++;
+_putchar(str[i]);
+i++;
 }
 
 }
@@ -26,24 +26,24 @@ while (str[i])
 
 int _atoi(const char *s)
 {
-	int sign = 1;
-	unsigned long int resp = 0, firstNum, i;
+int sign = 1;
+unsigned long int resp = 0, firstNum, i;
 
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
-	{
-		if (s[firstNum] == '-')
-		{
-			sign *= -1;
-		}
-	}
+for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+{
+if (s[firstNum] == '-')
+{
+sign *= -1;
+}
+}
 
-	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-	{
-		resp *= 10;
-		resp += (s[i] - 48);
-	}
+for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+{
+resp *= 10;
+resp += (s[i] - 48);
+}
 
-	return (sign * resp);
+return (sign *resp);
 }
 
 /**
@@ -62,8 +62,8 @@ for (i = 0; n / divisor > 9; i++, divisor *= 10)
 
 for (; divisor >= 1; n %= divisor, divisor /= 10)
 {
-	resp = n / divisor;
-	_putchar('0' + resp);
+resp = n / divisor;
+_putchar('0' + resp);
 }
 
 }
@@ -81,8 +81,8 @@ int main(int argc, char const *argv[])
 
 if (argc != 3)
 {
-	_puts("Error ");
-	exit(98);
+_puts("Error ");
+exit(98);
 }
 print_int(_atoi(argv[1]) * _atoi(argv[2]));
 _putchar('\n');
