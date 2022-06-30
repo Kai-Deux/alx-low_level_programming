b9d9249eac7855cd3ed199ce694262b293d98c74
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-06-30 21:35:09 +0200

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Auth: Kai Deux
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0C-more_malloc_free directory.
+ */
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -8,4 +15,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif /*MAIN_H*/
+#endif 
