0ba9eb04ced00c88c4b6b5dd881eb0adbd449e65
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-06-30 14:51:57 +0200

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,15 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _puts(char *str);
+#include <stdlib.h>
+
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _putchar(char c);
-int _atoi(const char *s);
-void print_int(unsigned long int n);
-
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
 #endif
