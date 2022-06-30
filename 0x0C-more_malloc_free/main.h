824e49be484c73777d967a6157c89be5b21edb4d
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-06-30 20:59:05 +0200

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,10 +1,27 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+/*header file for 0x0B*/
+
+/*as always*/
+int _putchar(char);
+
+/*question 0*/
 void *malloc_checked(unsigned int b);
+
+/*question 1*/
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+/*question 2*/
 void *_calloc(unsigned int nmemb, unsigned int size);
+
+/*question 3*/
 int *array_range(int min, int max);
+
+/*question 4*/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+
+char *_mul(char *s1, char *s2);
 
 #endif
