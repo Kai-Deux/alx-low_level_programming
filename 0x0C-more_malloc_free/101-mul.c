409479dd70e79153b88a23fbfadca8ff24bbe712
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-07-01 12:37:22 +0200

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,100 +1,135 @@
-#include <string.h>
+#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
-/**
- * _isnumber - checks if string is number
- *
- * @s: string
- *
- * Return: 1 if number, 0 if not
- */
-int _isnumber(char *s)
-{
-	int i, check, d;
 
-	d = 0, check = 1;
-	for (i = 0; *(s + i) != 0; i++)
+/**
+ * _memset - fills memory with a constant byte
+ *
+ * @s: input pointer that represents memory block
+ *     to fill
+ * @b: characters to fill/set
+ * @n: number of bytes to be filled
+ *
+ * Return: pointer to the filled memory area
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
 	{
-		d = isdigit(*(s + i));
-		if (d == 0)
-		{
-			check = 0;
-			break;
-		}
+		s[i] = b;
+		i++;
 	}
-	return (check);
+	return (s);
 }
 
 /**
- * _callocX - reserves memory initialized to 0
+ * _calloc - function that allocates memory
+ *           for an array using memset
  *
- * @nmemb: # of bytes
+ * @nmemb: size of array
+ * @size: size of each element
  *
- * Return: pointer
- */
-char *_callocX(unsigned int nmemb)
-{
-	unsigned int i;
-	char *p;
+ * Return: pointer to new allocated memory
+*/
 
-	p = malloc(nmemb + 1);
-	if (p == 0)
-		return (0);
-	for (i = 0; i < nmemb; i++)
-		p[i] = '0';
-	p[i] = '\0';
-	return (p);
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
 }
 
+
 /**
- * main - multiplies inf numbers
+ * multiply - initialize array with 0 byte
  *
- * @argc: # of cmd line args
- * @argv: cmd line args
- * Return: No return
- */
-int main(int argc, char **argv)
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: nothing
+*/
+
+void multiply(char *s1, char *s2)
 {
-	int i, j, l1, l2, lful, mul, add, ten, ten2, tl, zer = 0;
-	char *res;
+	int i, l1, l2, total_l, f_digit, s_digit, res = 0, tmp;
+	char *ptr;
+	void *temp;
 
-	if (argc != 3 || _isnumber(argv[1]) == 0 || _isnumber(argv[2]) == 0)
-		printf("Error\n"), exit(98);
+	l1 = _length(s1);
+	l2 = _length(s2);
+	tmp = l2;
+	total_l = l1 + l2;
+	ptr = _calloc(sizeof(int), total_l);
 
-	if (atoi(argv[1]) == 0 || atoi(argv[2]) == 0)
-		printf("0\n"), exit(0);
-	l1 = strlen(argv[1]), l2 = strlen(argv[2]);
-	lful = l1 + l2;
-	res = _callocX(lful);
+	/* store our pointer address to free later */
+	temp = ptr;
 
-	if (res == 0)
-		printf("Error\n"), exit(98);
-
-	for (i = l2 - 1; i >= 0; i--)
+	for (l1--; l1 >= 0; l1--)
 	{
-		ten = 0, ten2 = 0;
-		for (j = l1 - 1; j >= 0; j--)
+		f_digit = s1[l1] - '0';
+		res = 0;
+		l2 = tmp;
+		for (l2--; l2 >= 0; l2--)
 		{
-			tl = i + j + 1;
-			mul = (argv[1][j] - '0') * (argv[2][i] - '0') + ten;
-			ten =  mul / 10;
-			add = (res[tl] - '0') + (mul % 10) + ten2;
-			ten2 = add / 10;
-			res[tl] = (add % 10) + '0';
+			s_digit = s2[l2] - '0';
+			res += ptr[l2 + l1 + 1] + (f_digit * s_digit);
+			ptr[l1 + l2 + 1] = res % 10;
+			res /= 10;
 		}
-		res[tl - 1] = (ten + ten2) + '0';
+		if (res)
+			ptr[l1 + l2 + 1] = res % 10;
 	}
 
-	if (res[0] == '0')
-		zer = 1;
+	while (*ptr == 0)
+	{
+		ptr++;
+		total_l--;
+	}
 
-	for (; zer < lful; zer++)
-		printf("%c", res[zer]);
+	for (i = 0; i < total_l; i++)
+		printf("%i", ptr[i]);
 	printf("\n");
-
-	free(res);
-
-	return (0);
-
+	free(temp);
 }
+
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that multiplies
+ *            two positive numbers
+ *
+ * @argc: number of arguments
+ * @argv: arguments array
+ *
+ * Return: 0 on success 98 on faliure
+*/
+
+int main(int argc, char *argv[])
+{
+	char *n1 = argv[1];
+	char *n2 = argv[2];
+
+	if (argc != 3 || check_number(n1) || check_number(n2))
+		error_exit();
+
+	if (*n1 == '0' || *n2 == '0')
+	{
+		_putchar('0');
+		_putchar('\n');
+	}
+	else
+		multiply(n1, n2);
+	return (0);
+}
+
