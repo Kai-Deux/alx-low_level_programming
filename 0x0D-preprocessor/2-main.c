994cc088ce2de9f7155b75a4195b62b95ac71768
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-07-04 12:04:27 +0200

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,10 @@
-#ifndef _FUNCTION_LIKE_MACRO_
-#define _FUNCTION_LIKE_MACRO_
-
-#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
-
-#endif
+#include <stdio.h>
+/**
+ * main - prints the name of the file
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
