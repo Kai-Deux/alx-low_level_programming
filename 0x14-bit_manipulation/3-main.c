79f7ddb87130f23825732662c89d3d4b7b6a91db
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-07-21 18:08:33 +0200

    Update 3-main.c

--- 0x14-bit_manipulation/3-main.c
+++ 0x14-bit_manipulation/3-main.c
@@ -11,13 +11,13 @@ int main(void)
     unsigned long int n;
 
     n = 1024;
-    clear_bit(&n, 10);
+    set_bit(&n, 5);
     printf("%lu\n", n);
     n = 0;
-    clear_bit(&n, 10);
+    set_bit(&n, 10);
     printf("%lu\n", n);
     n = 98;
-    clear_bit(&n, 1);
+    set_bit(&n, 0);
     printf("%lu\n", n);
     return (0);
 }
