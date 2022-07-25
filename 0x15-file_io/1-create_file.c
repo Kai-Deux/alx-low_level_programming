bbc3dbf65c97be3638bfbe30e357f031fd3e88d9
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-07-25 19:07:19 +0200

    Task 1: Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * create_file - create a file and write in it
+ * @filename: file to be created
+ * @text_content: content of the file created
+ * Return: 1 if success -1 if fail
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len, w;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
+		w = write(fd, text_content, len);
+	}
+	if (w == -1)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
