8e9a48f6317b0f783e5b45f50e054b29f2bde609
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-07-25 19:15:16 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,43 @@
 #include "main.h"
-
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: pointer to text in a file
- * @letters: number of letters
- * Return: the actual number of letters it could read and print
- */
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t file, fread, fwrite;
-char *totalSize;
+	int file;
+	ssize_t read_check, wcount;
+	char *buffer;
 
-totalSize = malloc(sizeof(char) * letters);
-if (totalSize == NULL)
-return (0);
-if (filename == NULL)
-return (0);
+	if (filename == NULL) /*check if file is present*/
+		return (0);
 
-file = open(filename, O_RDONLY);
-if (file == -1)
-return (0);
-fread = read(file, totalSize, letters);
-if (fread == -1)
-return (0);
-fwrite = write(STDOUT_FILENO, totalSize, fread);
-if (fwrite == -1)
-return (0);
-close(file);
-free(totalSize);
-return (fwrite);
+	file = open(filename, O_RDONLY); /*open file*/
+
+	if (file == -1)
+		return (0);
+
+	/*get the size of buffer from number of letters*/
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	read_check = read(file, buffer, letters); /*read file*/
+	if (read_check == -1) /*check if read failed*/
+		return (0);
+
+	wcount = write(STDOUT_FILENO, buffer, read_check); /*write to POSIX*/
+	if (wcount == -1 || read_check != wcount) /*check if write failed*/
+		return (0);
+
+	free(buffer);
+
+	close(file); /*close file*/
+
+	return (wcount);
 }
