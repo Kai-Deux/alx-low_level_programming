902c878dde8fc605fdb3f2fe51b6f5c382b0797b
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-07-25 19:01:14 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,44 @@
 #include "main.h"
-/**
- * read_textfile - reads text file and prints it to stdout
- * @filename: Name of file to be read
- * @letters: exact number of letter to be printed
- * Return: 0 if it fails, else the number of characters
- */
 
+/**
+ * read_textfile - reads a text file and prints it to the POSIX stdout
+ * @filename: name of file to be read/printed
+ * @letters: number of letters it should read and print
+ * Return: actual number of letters it could read and print, 0 on failure
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	/* Declare variables to be used */
 	int fd;
-	ssize_t numread, numwrit;
-	char *buf;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-	/* open file */
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (buffer == NULL)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
-	/* check if file opens */
 	if (fd == -1)
 		return (0);
-	/* malloc space */
-	buf = malloc(letters);
-	if (buf == NULL)
-		return (0);
-	/* Now proceed to read the text file */
-	numread = read(fd, buf, letters);
-	/* write to stdout */
-	numwrit = write(STDOUT_FILENO, buf, numread);
-	free(buf);
+
+	bytes_read = read(fd, buffer, letters);
 	close(fd);
-	return (numwrit);
+
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written != bytes_read)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	return (bytes_written);
 }
