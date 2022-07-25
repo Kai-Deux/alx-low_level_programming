15fcb58d6e823486cf238ac64e834400cf4e69e6
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-07-25 19:22:54 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,39 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
- * Return: the number of letters printed, or 0 if it failed
-*/
+ * read_textfile - reads a text file and prints in POSIX stdout
+ * @filename: pointer to the name of the file
+ * @letters: The number of letters the function
+ *           should read and print.
+ *
+ * Return: if filename is NULL return 0
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file;
-	ssize_t read_check, wcount;
-	char *buffer;
+ssize_t o, r, w;
+char *buffer;
 
-	if (filename == NULL) /*check if file is present*/
-		return (0);
+if (filename == NULL)
+return (0);
 
-	file = open(filename, O_RDONLY); /*open file*/
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
 
-	if (file == -1)
-		return (0);
+o = open(filename, O_RDONLY);
+r = read(o, buffer, letters);
+w = write(STDOUT_FILENO, buffer, r);
 
-	/*get the size of buffer from number of letters*/
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		free(buffer);
-		return (0);
-	}
+if (o == -1 || r == -1 || w == -1 || w != r)
+{
+free(buffer);
+return (0);
+}
 
-	read_check = read(file, buffer, letters); /*read file*/
-	if (read_check == -1) /*check if read failed*/
-		return (0);
+free(buffer);
+close(o);
 
-	wcount = write(STDOUT_FILENO, buffer, read_check); /*write to POSIX*/
-	if (wcount == -1 || read_check != wcount) /*check if write failed*/
-		return (0);
-
-	free(buffer);
-
-	close(file); /*close file*/
-
-	return (wcount);
+return (w);
 }
