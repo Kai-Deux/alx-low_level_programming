ef10e228949771af0a5acb77416776aad433db23
Kai-Deux <rohisharamauthar@gmail.com>
2022-09-05 05:43:12 -0700

    Task 0

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+
+ * main - check the code
+
+ *
+
+ * Return: Always EXIT_SUCCESS.
+
+ */
+
+int main(void)
+
+{
+
+    printf("%d\n", _strlen("My Dyn Lib"));
+
+    return (EXIT_SUCCESS);
+
+}
